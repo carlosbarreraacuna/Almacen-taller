ede4c5bb15c00252183af00f6fb7dcece8899704
carlosbarreraacuna <carlos.barreraacuna@gmail.com>
2023-10-06 08:59:44 -0500

    creando componentes con livewire - apartado items y el contenido del items

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mt-5 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                 <x-welcome />
+                 <livewire:items />
             </div>
         </div>
     </div>
---
+++ resources/views/livewire/items.blade.php
@@ -0,0 +1,3 @@
+<div>
+    esta es una prueba
+</div>
